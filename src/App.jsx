d4dcd137df2f0aa0bb67bdff250a189bf99be661
Léo LCL. CLERC <lcl@mtb.aigle>
2025-12-10 22:33:31 +0100

    Remove custom cursor and restore default system cursor

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,14 @@ import Navbar from './Navbar';
 import Home from './pages/Home';
 import Skills from './pages/Skills';
 import Projects from './pages/Projects';
-import CustomCursor from './components/CustomCursor';
+import Projects from './pages/Projects';
 import './index.css';
 
 export default function App() {
   const location = useLocation();
 
   return (
-    <div className="app-container cursor-none"> {/* Hide default cursor */}
-      <CustomCursor />
+    <div className="app-container">
       <Navbar />
       <div className="stars-bg">
         <div className="overlay" />
